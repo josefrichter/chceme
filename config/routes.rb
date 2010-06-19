9c348f0fa7637a4dd7ae0a18eda8606625b16565
Josef Richter <josef.richter@me.com>
2010-06-19 14:04:40 +0200

    Document model generated, slight changes to database configuration to make it run

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Chceme::Application.routes.draw do |map|
+  resources :documents
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +51,7 @@ Chceme::Application.routes.draw do |map|
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => "documents#index"
 
   # See how all your routes lay out with "rake routes"
 
